a841a374dff2540ab8978e761ff13b50861cd75a
lecong <lecong.it@gmail.com>
2024-05-31 14:46:56 +0700

    Updates

--- _app/immutable/entry/start.CVLXhX97.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.B0ix6QnE.js";export{t as start};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sveltekit-pwa/sw.js', { scope: '/sveltekit-pwa/' })})}
